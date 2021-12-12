9305e6c4cb1d71467782c99949af6ff9145e2d7f
CarloArdito95 <93673652+CarloArdito95@users.noreply.github.com>
2021-12-12 18:46:09 +0100

    Add files via upload

--- python_analysis.py
+++ python_analysis.py
@@ -8,6 +8,7 @@ Created on Sat Dec 11 17:38:11 2021
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 forecast = pd.read_csv("C:/Users/cosim/Desktop/Querini/Data visualization/Forecast_merged.csv")
 os.chdir("C:/Users/cosim/Desktop/Querini/Data visualization")
@@ -32,16 +33,30 @@ colors = {'Rome':'red', 'Reykjavik':'blue','Jerusalem':'brown','Barcelona':'yell
           'San Martino in Pensilis':'pink','Schiavonea':'green','Berlino':'lime','Budapest':'violet','Bruxelles':'sandybrown','Malta':'gold',
           'Parigi':'indigo'}
 
-forecast.plot( kind="scatter", x="time", y="temperature", c = forecast["city"].map(colors))
-plt.xticks("time","")
+
+ax=forecast.plot( kind="scatter", x="time", y="temperature", c = forecast["city"].map(colors))
+plt.xticks('')
 plt.tight_layout()
 plt.savefig( "Temepratures")
 plt.show()
 
-
+#%%
 
 #TEMPERATURA A ROMA
 
+figure, axes = plt.subplots(nrows=3, ncols=3)
+
+axes[0, 0].plot(x, y)
+#create specific subplots
+
+axes[0, 1].plot(x1, np.sin(x1))
+axes[1, 0].plot(x1, np.cos(x1))
+axes[1, 1].plot(range(10))
+
+figure.tight_layout()
+
+for i in cities:
+    
 Rome = forecast[forecast["city"]=="Rome"]
 
 Rome.plot( kind="scatter", x="time", y="temperature")
